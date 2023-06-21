ea091991c193f83d86ed2eaaecdebf6534438c49
Dell <kaviprajapati2003@gmail.com>
2023-06-21 18:08:36 +0530

    Add Login Class

---
+++ Models/LoginClass.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubProject.Models
+{
+    public class LoginClass
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
