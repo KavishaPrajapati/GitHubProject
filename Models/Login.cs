5ea15e37c42bf7252cccab62eac7edb86ccea61f
Dell <kaviprajapati2003@gmail.com>
2023-06-22 14:30:57 +0530

    doine

--- Models/Login.cs
+++ Models/Login.cs
@@ -3,7 +3,7 @@
     public class Login
     {
         public int id { get; set; }
-        public string UserName { get; set; }
+        public string username { get; set; }
         public string password { get; set; }
     }
 }
