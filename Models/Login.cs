73ff42faae15f3220dada3eae95d4b49c7a572ee
Dell <kaviprajapati2003@gmail.com>
2023-06-22 13:01:00 +0530

    Subtract

---
+++ Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace GitHubProject.Models
+{
+    public class Login
+    {
+        public int id { get; set; }
+        public string UserName { get; set; }
+        public string password { get; set; }
+    }
+}
